83450757f1f5cf930240f8db89c6b6534592b9fc
Jeremias Pereira da Silva Junior <jeremias@email.com>
2026-04-28 11:47:05 -0600

    Remove TEMPLATES setting from Django configuration

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -11,12 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-TEMPLATES = [
-    {
-        'DIRS': [BASE_DIR / 'templates'],
-    },
-]
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
