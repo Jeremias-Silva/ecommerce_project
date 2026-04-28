76a3143268e9fa49bbbd51eb3ebf96b1828e0052
Jeremias Pereira da Silva Junior <jeremias@email.com>
2026-04-28 12:15:05 -0600

    Fix syntax error in product_list view rendering

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def product_list(request):
-    return render(request, 'base.html, products.html')# Create your views here.
+    return render(request, 'base.html', products.html')# Create your views here.
+    return render(request, 'products.html')
