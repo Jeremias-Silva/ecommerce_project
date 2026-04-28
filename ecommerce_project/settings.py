dccc9c4c5ace0f0f44d0ef9240bffe85df95a04a
Jeremias Pereira da Silva Junior <jeremias@email.com>
2026-04-28 11:38:33 -0600

    Update template directory settings in Django configuration

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -16,7 +16,6 @@ TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'ecommerce_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
