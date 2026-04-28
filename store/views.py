22697a836fc755b452f78cb31fe9bab9638f018e
Jeremias Pereira da Silva Junior <jeremias@email.com>
2026-04-28 12:16:18 -0600

    Fix rendering logic in product_list view

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def product_list(request):
-    return render(request, 'base.html', products.html')# Create your views here.
-    return render(request, 'products.html')
+    return render(request, 'base.html')# Create your views here.
+    return render(request, 'products.html')# Create your views here.
