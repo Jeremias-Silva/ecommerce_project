a294c315156731a9fcddf5bee831ed273470abe9
Jeremias Pereira da Silva Junior <jeremias@email.com>
2026-04-30 18:07:49 -0600

    Fix URL mapping: update product list view reference and add product detail route

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products/', views.products, name='products'),
+    path('products/', views.product_list, name='products'),
     path('login/', views.login_view, name='login'),
     path('cart/', views.cart, name='cart'),
+    path('create/', views.product_create, name='product_create'),
+    path('products/<int:id>/', views.product_detail, name='product_detail'),
 ]
+
+
+
