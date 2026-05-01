b54281446d30451ff34fed2a916ab1b45ad6a81e
Jeremias Pereira da Silva Junior <jeremias@email.com>
2026-04-30 22:08:50 -0600

    Add registration form: create RegisterForm for user registration with additional fields

--- store/forms.py
+++ store/forms.py
@@ -1,7 +1,38 @@
 from django import forms
-from .models import Product
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Product, CustomerProfile
 
+
+# PRODUCTS
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'description']
+
+
+# REGISTER
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    city = forms.CharField(max_length=100)
+    address = forms.CharField(max_length=255)
+    phone = forms.CharField(max_length=20)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+            CustomerProfile.objects.create(
+                user=user,
+                city=self.cleaned_data['city'],
+                address=self.cleaned_data['address'],
+                phone=self.cleaned_data['phone']
+            )
+
+        return user
